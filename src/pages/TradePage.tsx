e9d0caceedd472e550f9a97f6c3ba137961e3b58
tigthor <tigthor1337@gmail.com>
2021-05-10 16:52:35 +0300

    Making charts work

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Orderbook from '../components/Orderbook';
 import UserInfoTable from '../components/UserInfoTable';
 import StandaloneBalancesDisplay from '../components/StandaloneBalancesDisplay';
-import {TVChartContainer} from '../components/TradingView/index';
+import { TVChartContainer } from '../components/TradingView/index';
 import {
   getMarketInfos,
   getTradePageUrl,
@@ -56,7 +56,7 @@ export default function TradePage() {
       marketAddress={marketAddress}
       setMarketAddress={setMarketAddress}
     >
-      <TVChartContainer/>
+      <TVChartContainer />
       <TradePageInner />
     </MarketProvider>
   );
@@ -150,6 +150,7 @@ function TradePageInner() {
 
   return (
     <>
+      <TVChartContainer />
       <CustomMarketDialog
         visible={addMarketVisible}
         onClose={() => setAddMarketVisible(false)}
@@ -286,15 +287,15 @@ function MarketSelector({
               ? -1
               : extractQuote(a.name) !== 'USDT' &&
                 extractQuote(b.name) === 'USDT'
-              ? 1
-              : 0,
+                ? 1
+                : 0,
           )
           .sort((a, b) =>
             extractBase(a.name) < extractBase(b.name)
               ? -1
               : extractBase(a.name) > extractBase(b.name)
-              ? 1
-              : 0,
+                ? 1
+                : 0,
           )
           .map(({ address, name, deprecated }, i) => (
             <Option
